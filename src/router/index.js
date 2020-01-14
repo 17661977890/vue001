8576d3568bf7ecff30bb57e6cdef0aa1391bc7c8
sunbin <17661977890@163.com>
2020-01-14 16:17:48 +0800

    新建页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Hi from '@/components/Hi'
 
 Vue.use(Router)
-
+// 新建页面需要创建对应的路由
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/hi',
+      name: 'Hi',
+      component: Hi
     }
+
   ]
 })
